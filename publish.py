20707c5dcf01ece65affa81112ea4989b1869dff
rendaw <>
2018-10-27 14:12:08 +0900

    Final packaging script

--- publish.py
+++ publish.py
@@ -4,6 +4,7 @@ import logging
 import argparse
 import os
 import re
+import lxml.etree
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -13,7 +14,13 @@ args = parser.parse_args()
 if not re.match('\\d+\\.\\d+\\.\\d+', args.version):
     args.error('version must be in the format N.N.N')
 
-if subprocess.call(['git', 'diff-index', '--quiet', 'HEAD']) != 0:  # noqa
+jenv = os.environ.copy()
+jenv['JAVA_HOME'] = '/usr/lib/jvm/java-8-jdk'
+subprocess.check_call(['mvn', 'clean', 'verify', '-P', 'prerelease'], env=jenv)
+
+if subprocess.call([
+    'git', 'diff', '--exit-code', '--quiet', 'HEAD'
+]) != 0:
     raise RuntimeError('Working directory must be clean.')
 
 
@@ -29,12 +36,29 @@ def replace(path, *replacements):
     os.rename(temp, path)
 
 
-mvnver = [
-    '<artifactId>gettus</artifactId>([^<]*)<version>[^<]+</version>',  # noqa
-    '<artifactId>gettus</artifactId>\\1<version>{}</version>'.format(args.version),  # noqa
-]
-replace('readme.md', mvnver)
-replace('package/pom.xml', mvnver)
+artifacts = []
+
+for root, dirs, files in os.walk('.'):
+    for f in files:
+        if f != 'pom.xml':
+            continue
+        path = os.path.join(root, f)
+        doc = lxml.etree.parse(path)
+        root = doc.getroot()
+        pomver = root.find('version', namespaces=root.nsmap)
+        if pomver.text == '0.0.0':
+            continue
+        artifacts.append(root.find('artifactId', namespaces=root.nsmap).text)
+        pomver.text = args.version
+        doc.write(path)
+
+
+replace('readme.md', *[
+    [
+        '<artifactId>{}</artifactId>([^<]*)<version>[^<]+</version>'.format(artifact),  # noqa
+        '<artifactId>{}</artifactId>\\1<version>{}</version>'.format(artifact, args.version),  # noqa
+    ] for artifact in artifacts
+])
 subprocess.check_call([
     'git', 'commit', '-a', '-m', 'Update version: {}'.format(args.version)])
 
@@ -44,6 +68,5 @@ subprocess.check_call(['git', 'push', '--tags'])
 subprocess.check_call(['git', 'push'])
 
 # Build and publish
-env = os.environ.copy()
-env['JAVA_HOME'] = '/usr/lib/jvm/java-8-jdk'
-subprocess.check_call(['mvn', 'clean', 'deploy', '-P', 'release'], env=env)
+subprocess.check_call([
+    'mvn', 'clean', 'deploy', '-P', 'prerelease,release'], env=jenv)
